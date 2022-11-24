7da693bbd46b8a464f3b55d185092a590991a3cc
Nikula Daniil <daniil.nikula@gmail.com>
2022-11-24 17:55:32 +0200

    added User.cs class

--- Module 12. Algorithms Task 12.1.5/Program.cs
+++ Module 12. Algorithms Task 12.1.5/Program.cs
@@ -1,4 +1,5 @@
-﻿namespace Module_12._1_Algorithms
+﻿
+namespace Module_12._Algorithms_Task_12._1._5
 {
     internal class Program
     {
@@ -27,26 +28,4 @@
             }
         }
     }
-    class User
-    {
-        public string Login { get; set; }
-        public string Name { get; set; }
-        public bool IsPremium { get; set; }
-        public static void ShowAds()
-        {
-            Console.WriteLine("Посетите наш новый сайт с бесплатными играми free.games.for.a.fool.com");
-            // Остановка на 1 с
-            Thread.Sleep(1000);
-
-            Console.WriteLine("Купите подписку на МыКомбо и слушайте музыку везде и всегда.");
-            // Остановка на 2 с
-            Thread.Sleep(2000);
-
-            Console.WriteLine("Оформите премиум-подписку на наш сервис, чтобы не видеть рекламу.");
-            // Остановка на 3 с
-            Thread.Sleep(3000);
-        }
-    }
-
-
 }
---
+++ Module 12. Algorithms Task 12.1.5/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_12._Algorithms_Task_12._1._5
+{
+    class User
+    {
+        public string Login { get; set; }
+        public string Name { get; set; }
+        public bool IsPremium { get; set; }
+        public static void ShowAds()
+        {
+            Console.WriteLine("Посетите наш новый сайт с бесплатными играми free.games.for.a.fool.com");
+            // Остановка на 1 с
+            Thread.Sleep(1000);
+
+            Console.WriteLine("Купите подписку на МыКомбо и слушайте музыку везде и всегда.");
+            // Остановка на 2 с
+            Thread.Sleep(2000);
+
+            Console.WriteLine("Оформите премиум-подписку на наш сервис, чтобы не видеть рекламу.");
+            // Остановка на 3 с
+            Thread.Sleep(3000);
+        }
+    }
+}
